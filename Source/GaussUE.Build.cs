fe0b3350b5e3b55c0f585e685407ce6d739d58aa
whatphilipcodes <gerdes.philip@gmail.com>
2026-04-10 11:33:16 +0200

    fix: formatting

--- Source/GaussUE.Build.cs
+++ Source/GaussUE.Build.cs
@@ -2,25 +2,23 @@ using UnrealBuildTool;
 
 public class GaussUE : ModuleRules
 {
-	public GaussUE(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-			}
-		);
-			
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-			}
-		);
-	}
+    public GaussUE(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            [
+                "Core",
+            ]
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            [
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+            ]
+        );
+    }
 }
